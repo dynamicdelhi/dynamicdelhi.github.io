9e5bcde9405fbdafd467c5aac7052a9d896e40ad
Ankita Dhillon <Ankita Dhillon>
2017-08-07 14:53:33 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,5 +74,31 @@ $(document).ready(function(){
           $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(200);
         });
 
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+ //   if ($(".filter-button").removeClass("active")) {
+//$(this).removeClass("active");
+//}
+//$(this).addClass("active");
+
+});
 
 }); // end of $(document).ready(function()
